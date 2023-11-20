4b27d1f3a28120d8be9d9d682058804e406c4da8
Grace <82984540+GraceH26@users.noreply.github.com>
2023-11-19 21:37:18 -0500

    beggining notes

---
+++ notes.py
@@ -0,0 +1,82 @@
+#Strings
+#first_name = "Grace"
+#last_name = "Hall"
+#full_name = "Grace Hall"
+#print("hello " + full_name)
+
+
+
+#Integers
+#age = 21
+#age += 1
+#print("Your age is: "  +str(age))
+
+#float
+#height = 250.5
+
+#print("Your height is: "+str(height)+ "cm")
+#print(type(height))
+
+#bool
+#human = True
+#print("are you a human: "+str(human))
+#print(type(human))
+
+
+
+#multiple assignment = allows us to assign multiple variables at the same time in one line of code
+# name = "Grace"
+#age = 16
+#attractive = True
+
+#name, age, attractive = "Grace", 16, True
+
+#print(name)
+#print(age)
+#print(attractive)
+
+#Spongebob = Patrick = Sandy = Squidward = 30
+#print(Spongebob, Patrick, Sandy, Squidward)
+
+
+#name = "Grace"
+
+#print(len(name))
+#print(name.find("r"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isalpha())
+#print(name.count("r"))
+#print(name.replace("r", "a"))
+#print(name*3)
+
+
+
+#type casting = convert the date type of a value to another data type.
+
+#x = 1 #int
+#y = 2.0 #float
+#z = "3" #string
+
+#int(y)
+#z = int(z)
+#x = float(x)
+#y = str("Y is: "+str(y))
+#print(x)
+#print(y)
+#print(z*3)
+
+
+
+#input = input is user created strings or variables to add to your program
+
+#name = input("What is your name?: ")
+#age = int(input("How old are you?: "))
+#height = float(input("How tall are you?: "))
+
+#age = age 
+
+#print("Hello "+name)
+#print("You are "+str(age)+" years old")
+#print("You are "+str(height)+"cm tall")
